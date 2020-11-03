173499e17381df49f938fed52b63cdcf77af3fa9
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-11-02 19:33:15 -0600

    paginacion horizontal necesito pasarla vertical

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FirstView from './components/FirstView';
-import SecondView from './components/SecondView';
+import AllView from "./components/AllView";
 
 function App() {
   return (
-   /*  <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
       <div>
-        <FirstView/>
-        <SecondView/>
-      </div>  
-    // </div>
+        <AllView/>
+      </div>
   );
 }
 
--- src/components/AllView.js
+++ src/components/AllView.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
+import { Pagination } from "react-bootstrap";
+import FirstView from './FirstView';
+import SecondView from './SecondView';
+import ReactPageScroller from "react-page-scroller";
 
 export default class AllView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { currentPage: null };
+    }
+
+    handlePageChange = number => {
+        console.log(number);
+        this.setState({ currentPage: number });
+    };
+
+    getPagesNumbers = () => {
+        const pageNumbers = [];
+    
+        for (let i = 1; i <= 5; i++) {
+          pageNumbers.push(
+            <Pagination.Item key={i} eventKey={i - 1} onSelect={this.handlePageChange} active={i === this.state.currentPage + 1}>
+              {i}
+            </Pagination.Item>,
+          );
+        }
+    
+        return [...pageNumbers];
+    };
+
     render() {
+
+        const pagesNumbers = this.getPagesNumbers();
+
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <ReactPageScroller
+                    pageOnChange={this.handlePageChange}
+                    customPageNumber={this.state.currentPage}
+                    >
+                    <FirstView />
+                    <SecondView />
+                </ReactPageScroller>
+                <Pagination className="pagination-additional-class header-pos2" bsSize="large">
+                    {pagesNumbers}
+                </Pagination>
+            </React.Fragment>
         )
     }
 }
