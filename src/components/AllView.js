20e16fc7bf21cc2a0a2be95c3a3f576b87b7e9a2
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-11-03 09:52:15 -0600

    solamenta falta el menu lateral

--- src/components/AllView.js
+++ src/components/AllView.js
@@ -12,19 +12,37 @@ export default class AllView extends Component {
     }
 
     handlePageChange = number => {
-        console.log(number);
-        this.setState({ currentPage: number });
+        
+        if (number == 1) {
+            number = 2;
+        }
+        this.setState({ currentPage: number});
     };
 
     getPagesNumbers = () => {
         const pageNumbers = [];
-    
-        for (let i = 1; i <= 5; i++) {
-          pageNumbers.push(
-            <Pagination.Item key={i} eventKey={i - 1} onSelect={this.handlePageChange} active={i === this.state.currentPage + 1}>
-              {i}
-            </Pagination.Item>,
-          );
+        
+        let par = false;
+        let res = 0;
+        let text = 0;
+        let arbol = 0;
+        for (let i = 1; i <= 3; i++) {
+            par = (i%2) ? false : true;
+
+            // saltar numeracion
+            if (par == false && i !== 1) {
+                arbol += 1;
+            }
+
+            text = (par) ? "|" : i - arbol;
+        
+            if (i !== 10) {
+                pageNumbers.push(
+                    <Pagination.Item key={i} eventKey={i} onSelect={this.handlePageChange} active={i === this.state.currentPage + 1}>
+                        {text}
+                    </Pagination.Item>,
+                );
+            }
         }
     
         return [...pageNumbers];
@@ -43,7 +61,7 @@ export default class AllView extends Component {
                     <FirstView />
                     <SecondView />
                 </ReactPageScroller>
-                <Pagination className="pager-page verticalpagin">
+                <Pagination className="pager-page">
                     {pagesNumbers}
                 </Pagination>
             </React.Fragment>
