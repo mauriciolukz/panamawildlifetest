6980a3146a11a3f0cc2851480561c414c110e6fe
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-11-02 22:46:38 -0600

    unicamente mejorar diseño paginador de pagina y arreglar css mejor

--- src/components/FirstView.js
+++ src/components/FirstView.js
@@ -14,20 +14,22 @@ export default class FirstView extends Component {
                         src="./img/banner/Slider-1---Background.jpg"
                         />
                         <Carousel.Caption>
-                        <h3>HELPING WILDLIFE THRIVE BY PLANTING NATIVE TREES IN PANAMA</h3>
+                        <div className="movermasIz">
+                            <h3 className="headerTitle">HELPING WILDLIFE THRIVE BY PLANTING NATIVE TREES IN</h3>
+                        </div>
                         <div>
-                            <div className="row justify-content-center">
+                            <div className="row justify-content-end">
                                 <div className="col-sm-2">
-                                    <h2 className="text-center">502</h2>
-                                    <p className="parrafosBold text-center">IN DANGERED SPECIES</p>
+                                    <h2 className="text-center numberFont">502</h2>
+                                    <p className="primer text-center">IN DANGERED SPECIES</p>
                                 </div>
                                 <div className="col-sm-2">
-                                    <h2 className="text-center">29 M</h2>
-                                    <p className="parrafosBold text-center">LOST ACRES OF TREES</p>
+                                    <h2 className="text-center numberFont">29M</h2>
+                                    <p className="primer text-center">LOST ACRES OF TREES</p>
                                 </div>
                                 <div className="col-sm-2">
-                                    <h2 className="text-center">120</h2>
-                                    <p className="parrafosBold text-center">EXTINCT SPECIES</p>
+                                    <h2 className="text-center numberFont">120</h2>
+                                    <p className="primer text-center">EXTINCT SPECIES</p>
                                 </div>
                             </div>
                         </div>
@@ -43,8 +45,10 @@ export default class FirstView extends Component {
                         alt="Third slide"
                         />
                         <Carousel.Caption>
-                            <h3 className="text-left">FIGHT CLIMATE CHANGE</h3>
-                            <p className="ficha parrafosBold text-left">Clamatechange isan existaht</p>
+                            <h3 className="text-left headerTitle">FIGHT CLIMATE CHANGE</h3>
+                            <div className="movermasIz">
+                                <p className="ficha parrafosBold text-left pl-1">Climate change is an existential threat. As individuals, we can feel overwhelmed. You can make a difference by planting trees.</p>
+                            </div>
                             <div className="row text-left"><button type="button" class="btn btn-success">PLANT A NATIVE TREE</button></div>
                         </Carousel.Caption>
                     </Carousel.Item>
@@ -55,10 +59,10 @@ export default class FirstView extends Component {
                         alt="Third slide"
                         />
                         <Carousel.Caption>
-                            <h3 className="text-right">TACKLING THE NATURE CRISIS</h3>
+                            <h3 className="text-right headerTitle">TACKLING THE NATURE CRISIS</h3>
                             <div className="movermas">
-                                <p className="ficha text-right">By restoring</p>
-                                <p className="ficha text-right">Noasting header</p>
+                                <p className="ficha text-right parrafosBold">By restoring precious habitats & planting new native forest, we extend & create wildlife havens. </p>
+                                <p className="ficha text-right parrafosBold">Boosting biodiversity is a pragmatics and effective strategy to mitigate climate change.</p>
                             </div>
                             <div className="row justify-content-end"><button type="button" class="btn btn-success">PLANT A NATIVE TREE</button></div>
                         </Carousel.Caption>
--- src/components/SecondView.js
+++ src/components/SecondView.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
+import { Carousel } from 'react-bootstrap';
 
 export default class SecondView extends Component {
     render() {
         return (
-            <div>
-                Hola
-            </div>
+            <React.Fragment>
+                <div class="container h-100">
+                    <div class="row justify-content-center h-100">
+                        <div class="col justify-content-center align-self-center text-center">
+                            <h1 className="text-left colorHeader">OUR NEW APPROACH</h1>
+                            <p className="fichaverde text-left pl-1">We are appealing to the human love of animals by helping contributors pick a tree to support their preferred jungle animals and our communities.</p>
+                            <p className="fichaverde text-left pl-1">We will be selling native trees starting as low as $16. Each contribution will help plant a tree that will benefit a specific set of animals, enrich the rainforest, and boost </p>
+                            <div className="row text-left pl-5"><button type="button" class="btn colorBotton">VIEW ALL ANIMAL</button></div>
+                        </div>
+                        <div class="col align-self-center text-center">
+                            <Carousel>
+                                <Carousel.Item interval={2000}>
+                                    <img
+                                    className="rounded-circle"
+                                    src="./img/animal/Tucan.png"
+                                    width="400" height="400"/>
+                                </Carousel.Item>
+                                <Carousel.Item interval={2000}>
+                                    <img
+                                    className="rounded-circle"
+                                    src="./img/animal/Tiger.png"
+                                    width="400" height="400"/>
+                                </Carousel.Item>
+                                <Carousel.Item interval={2000}>
+                                    <img
+                                    className="rounded-circle"
+                                    src="./img/animal/Iguana.png"
+                                    width="400" height="400"/>
+                                </Carousel.Item>
+                                <Carousel.Item interval={2000}>
+                                    <img
+                                    className="rounded-circle"
+                                    src="./img/animal/Paca.png"
+                                    width="400" height="400"/>
+                                </Carousel.Item>
+                                <Carousel.Item interval={2000}>
+                                    <img
+                                    className="rounded-circle"
+                                    src="./img/animal/Sloth.png"
+                                    width="400" height="400"/>
+                                </Carousel.Item>
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
