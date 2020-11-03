daa79a792e8f007a0f56dc23d31f3196ec65f69e
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-11-03 12:35:54 -0600

    checkbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import { Navbar,Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 
 export default function Header() {
     return (
         <header className="menu">
             <Navbar bg="none" expand="lg" className="pl-5 border-top remove-padding-top-bottom">
                 <Navbar.Brand href="#home"><img className="d-block w-60" src="./img/Leaf Logo White.png" alt="First slide" width='150px' height='50px'/></Navbar.Brand>
-                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto topnav">
-                        <Nav.Link href="#home" >Guias de actividades</Nav.Link>
-                        <Nav.Link href="#home">Preguntas frecuentes</Nav.Link>
-                        <Nav.Link href="#home">Novedades</Nav.Link>
-                        <Nav.Link href="#home">Contacto</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse> */}
+                <div className="rightCheck"><BootstrapSwitchButton checked={true} size="xs" onlabel='EN' offlabel='ES' checked={false} onstyle="success" offstyle="info"/></div>
             </Navbar>
         </header>
     )
